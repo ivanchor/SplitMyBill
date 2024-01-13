75b58082cb03af9c0c2e61e4f3d22840c282c6b8
ivanchor <ichor85@gmail.com>
2024-01-12 18:18:00 -0800

    Added Main Page

---
+++ lib/Bill_Page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BillPage extends StatefulWidget {
+  const BillPage({super.key});
+
+  @override
+  State<BillPage> createState() => _BillPageState();
+}
+
+class _BillPageState extends State<BillPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
